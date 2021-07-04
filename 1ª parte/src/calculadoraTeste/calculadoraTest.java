784bda49cefbf551e0b1d7164f8bf97be4293e74
MaressaOliveira25 <86390616+MaressaOliveira25@users.noreply.github.com>
2021-07-04 18:32:50 -0300

    Resposta da 1ª Parte

--- 1ª parte/src/calculadoraTeste/calculadoraTest.java
+++ 1ª parte/src/calculadoraTeste/calculadoraTest.java
@@ -1,7 +1,5 @@
 package calculadoraTeste;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 import calculadora.calculadora;
@@ -11,17 +9,29 @@ class calculadoraTest {
 	@Test
 	void testCalcular() {
 		calculadora calculadora=new calculadora();
-		if(calculadora.Calcular(5.57)==407) {
-			System.out.println("O método retornou o resultado esperado:");
-		}
-		else {
-			System.out.println("O método não retornou o resultado esperado:");
-		}
 		if(calculadora.Calcular(1.0)==30) {
-			System.out.println("O método retornou o resultado esperado:");
+			System.out.println("O método retornou o resultado esperado");
 		}
 		else {
-			System.out.println("O método não retornou o resultado esperado:");
+			System.out.println("O método não retornou o resultado esperado");
+		}
+		if(calculadora.Calcular(2.24)==107) {
+			System.out.println("O método retornou o resultado esperado");
+		}
+		else {
+			System.out.println("O método não retornou o resultado esperado");
+		}
+		if(calculadora.Calcular(3.90)==237) {
+			System.out.println("O método retornou o resultado esperado");
+		}
+		else {
+			System.out.println("O método não retornou o resultado esperado");
+		}
+		if(calculadora.Calcular(5.57)==407) {
+			System.out.println("O método retornou o resultado esperado");
+		}
+		else {
+			System.out.println("O método não retornou o resultado esperado");
 		}
 		
 		
